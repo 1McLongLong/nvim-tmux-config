857feaab98e64b0912c6360c9d19449f7bc0d205
1McLongLong <85419730+1McLongLong@users.noreply.github.com>
2024-12-11 10:41:28 +0100

    Update chadrc.lua

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,6 +1,11 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'chocolate' }
+M.ui = {
+  theme = 'falcon',
+  statusline = {
+    theme = "vscode_colored"
+  }
+}
 M.plugins = "custom.plugins"
 return M
