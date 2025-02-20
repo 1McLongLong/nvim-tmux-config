b4d54453e18f36e00a7fe6322f438b0e99334314
1McLongLong <85419730+1McLongLong@users.noreply.github.com>
2025-02-20 10:30:22 +0100

    Update plugins.lua

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,32 +1,46 @@
 local plugins = {
   {
-    {
-      "Diogo-ss/42-header.nvim",
-      cmd = { "Stdheader" },
-      keys = { "<F1>" },
-      opts = {
-        default_map = true, -- Default mapping <F1> in normal mode.
-        auto_update = true, -- Update header when saving.
-        user = "touahman", -- Your user.
-        mail = "touahman@student.1337.ma", -- Your mail.
-        -- add other options.
-      },
-      config = function(_, opts)
-        require("42header").setup(opts)
-      end,
+    "Diogo-ss/42-header.nvim",
+    cmd = { "Stdheader" },
+    keys = { "<F1>" },
+    opts = {
+      default_map = true, -- Default mapping <F1> in normal mode.
+      auto_update = true, -- Update header when saving.
+      user = "touahman", -- Your user.
+      mail = "touahman@student.1337.ma", -- Your mail.
+      -- add other options.
     },
-    {
-      "neovim/nvim-lspconfig",
-      config = function ()
-        require "plugins.configs.lspconfig"
-        require "custom.configs.lspconfig"
-      end,
-    },
+    config = function(_, opts)
+      require("42header").setup(opts)
+    end,
+  },
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+    config = function()
+      vim.g.codeium_no_map_tab = true
+      vim.keymap.set('i', '<c-y>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
         "clangd"
       }
+    }
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+     opts = {
+      auto_install = true,
     }
   }
 }
