38b5487a1bf248d20cef624afd14eba9b4c991b8
1McLongLong <85419730+1McLongLong@users.noreply.github.com>
2025-06-26 02:04:24 +0100

    Update plugins.lua

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -42,6 +42,13 @@ local plugins = {
      opts = {
       auto_install = true,
     }
-  }
+  },
+  {
+    "aurum77/live-server.nvim",
+    build = function()
+      require("live_server.util").install()
+    end,
+    cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
+  },
 }
 return plugins
